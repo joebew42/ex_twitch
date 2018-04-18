04f7091dcb7e6bfb68cdf532d52ae228b8697b40
JoeBew42 <joebew42@gmail.com>
2018-04-18 19:25:27 +0200

    initial commit

---
+++ lib/ex_twitch.ex
@@ -0,0 +1,18 @@
+defmodule ExTwitch do
+  @moduledoc """
+  Documentation for ExTwitch.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExTwitch.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_twitch_test.exs
@@ -0,0 +1,8 @@
+defmodule ExTwitchTest do
+  use ExUnit.Case
+  doctest ExTwitch
+
+  test "greets the world" do
+    assert ExTwitch.hello() == :world
+  end
+end
