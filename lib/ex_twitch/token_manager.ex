703b27bb602defcc13df8a9a22b191f8ba2ff4ec
JoeBew42 <joebew42@gmail.com>
2018-04-23 19:51:56 +0200

    extract the name of the token manager

--- lib/ex_twitch/token_manager.ex
+++ lib/ex_twitch/token_manager.ex
@@ -1,8 +1,10 @@
 defmodule ExTwitch.TokenManager do
   use GenServer
 
+  @token_manager :ex_twitch_token_manager
+
   def start_link(_opts) do
-    GenServer.start_link(__MODULE__, [], name: :ex_twitch_token_manager)
+    GenServer.start_link(__MODULE__, [], name: @token_manager)
   end
 
   def init(args) do
@@ -10,7 +12,7 @@ defmodule ExTwitch.TokenManager do
   end
 
   def token() do
-    GenServer.call(:ex_twitch_token_manager, :token)
+    GenServer.call(@token_manager, :token)
   end
 
   def handle_call(:token, _from, state) do
